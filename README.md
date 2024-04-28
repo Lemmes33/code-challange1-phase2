3a25107bb8e052ca693fc3edc17516b9e142dc6f
lemmes33 <vinlemuel021@gmail.com>
2024-04-28 20:21:16 +0300

    read.md

--- README.md
+++
@@ -1,1 +0,0 @@
-# code-challange1-phase2
--- bank-of-flatiron/src/App.js
+++ bank-of-flatiron/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Searchbox from './Searchbox';
+import './searchbox.css'
+import DatePicker from './DatePicker'
+import Description from './Description';  
+import Category from './Category';
+import Amount from './Amount';
+import Button from './Button';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <div className='title'> 
+     <h1>The Royal Bankof Flatiron</h1></div>
+      {/* search box */}
+      <Searchbox />
+      {/* date*/}
+      <DatePicker />
+      {/* description*/}
+      <Description />
+      {/* category*/}
+      <Category />
+      {/* amount*/}
+      <Amount />
+      {/* button */}
+      <Button />
     </div>
   );
 }
