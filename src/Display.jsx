f83abb67d7c687ae42889abfdad26570e4acfc9a
lemmes33 <vinlemuel021@gmail.com>
2024-05-02 13:08:47 +0300

    read.md

--- src/Display.jsx
+++ src/Display.jsx
@@ -8,29 +8,46 @@ function Display() {
   <table className="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Date</th>
+      <th scope="col">Description</th>
+      <th scope="col">Category</th>
+      <th scope="col">Amount</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row">2019-12-01</th>
+      <td>Paycheck from Bob's Burgers</td>
+      <td>Income</td>
+      <td>1000</td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <th scope="row">2019-12-01</th>
+      <td>South by Southwest quinoa Bowl at Fresh & Co</td>
+      <td>Food</td>
+      <td>-10.55</td>
     </tr>
     <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
+      <th scope="row">2019-12-02</th>
+      <td colspan="2">South by Southwest quinoa Bowl at Fresh & Co</td>
+      {/* <td>Food</td> */}
+      <td>-10.55</td>
+    </tr>
+    <tr>
+      <th scope="row">2019-12-04</th>
+      <td colspan="2">Sunglasses Urban Outfitters</td>
+      
+      <td>-24.99</td>
+    </tr>
+    <tr>
+      <th scope="row">2019-12-06</th>
+      <td colspan="2">Venmo, Allice pays you for Burito</td>
+      <td>8.75</td>
+    </tr>
+    <tr>
+      <th scope="row">2019-12-06</th>
+      <td colspan="2">Chipotle</td>
+      <td>-17.59</td>
     </tr>
   </tbody>
 </table>
