b56ab364ccaadd7391ab0dd843babe065663bff7
lemmes33 <vinlemuel021@gmail.com>
2024-05-05 13:02:00 +0300

    commits

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import Searchbox from './Searchbox';
-import './searchbox.css'
-import DatePicker from './DatePicker'
-import Description from './Description';  
-import Category from './Category';
-import Amount from './Amount';
-import Button from './Button';
-import Display from './Display';
-import List from './List';
-
-
+import OpenSource from './OpenSourceTable'
 function App() {
   return (
-    <div>
-     <div className='title'> 
-     <h1>The Royal Bankof Flatiron</h1></div>
-      {/* search box */}
-      <Searchbox />
-      {/* date*/}
-      <DatePicker />
-      {/* description*/}
-      {/* <Description /> */}
-      {/* category*/}
-      {/* <Category /> */}
-      {/* amount*/}
-      <Amount />
-      {/* button */}
-      <Button />
-      {/* display */}
-      <Display />
-      
+    <div className="App">
+      <header className="App-header">
+       
+        <OpenSource/>
+      </header>
     </div>
   );
 }
